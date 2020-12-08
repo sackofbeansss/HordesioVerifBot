cb8a002a980322e9824e96ec1218787c5a88380a
sackofbeansss <61995281+sackofbeansss@users.noreply.github.com>
2020-12-08 18:20:43 -0500

    bot.js prefix update

--- bot.js
+++ bot.js
@@ -10,29 +10,28 @@ client.on('ready', () => {
 
 });
 
- 
-
+const prefix = "+";
 client.on('message', message => {
-
-    if (message.content === ';ping') {
+    if (!message.content.startsWith(prefix)) return;
+    if (message.content.startsWith === (prefix + 'ping') {
 
        message.reply('pong');
 
-       }
+       } else
  
-    if (message.content === ';help') {
+    if (message.content.startsWith === (prefix + 'help') {
 
-       message.reply(';help, ;ping, ;verify, ;version');
+       message.reply('+help, +ping, +verify, +version');
 
-       }
+       } else
 
-    if (message.content === ';verify') {
+    if (message.content.startsWith === (prefix + 'verify') {
 
        message.reply('[insert message embed]');
 
-       }
+       } else
 
-    if (message.content === ';version') {
+    if (message.content.startsWith === (prefix + 'version') {
 
        message.reply('version 0.0.1');
 
